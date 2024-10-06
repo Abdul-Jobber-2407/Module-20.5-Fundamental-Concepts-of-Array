947e244f9ff04ce2655cebb37a1c69dcac80d629
AR Abdul Jobber <arjobbar2019@gmail.com>
2024-10-06 10:49:19 +0600

    first commit

---
+++ test.js
@@ -0,0 +1,41 @@
+// primitive
+//basic, primary data
+const age = 21;
+const school = 'Gokulkhali High School';
+const isPassed = true;
+let isDeveloper;
+
+console.log(isDeveloper);
+
+
+
+const age = 21;
+const school = 'Gokulkhali High School';
+const isPassed = true;
+
+const subjects = ['bangla', 'English', 'physics', 'Math']
+const bottle_wrong = [ 'white', 45];
+const bottleColor = 'white';
+const bottlePrice = 45;
+const bottleDrinks = 'water';
+
+//object\
+// non-primitive
+const bottle = {
+    brand : 'apple',
+    price: 45,
+    color: 'white',
+    isClean: false,
+}
+
+// non-primitive
+const subject = {
+name: 'biology',
+teacher: 'Kuddus sir',
+examDate: '30 Feb',
+chapters: ['first', 'second', 'third'],
+exam: {
+    name : 'Final Exam',
+    marks: 100,
+}
+}
